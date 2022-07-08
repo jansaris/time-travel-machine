9984bbce358d725aead6602336e75866b8b4e428
Jan Saris <jan.saris@gmail.com>
2022-07-08 14:00:53 +0200

    Fixed entering data

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ media.addEventListener('ended', stopMedia);
 document.addEventListener('keyup', handleKey);
 
 let run = false;
-let year = "2022";
-let bc = false;
+let year = "";
 
 function playPauseMedia() {
     if(media.paused) {
       run = true;
       button.setAttribute('data-icon','u');
+      media.playbackRate = 1;
       media.play();
       startAnimation();
     } else {
@@ -35,41 +35,55 @@ function stopMedia() {
 function handleKey(e) {
     if (run) return;
     e = e || window.event;
-    if (e.keyCode === 27 || e.keyCode === 46 || e.keyCode === 8 || e.keyCode === 110) year = "";
-    else if (e.key === '-') bc = !bc;
+    if (e.keyCode === 27 || e.keyCode === 46 || e.keyCode === 8 || e.keyCode === 110) {
+        year = "";
+        bc = false;
+    }
+    else if (e.key === '-') year = "" + (Number(year) * -1);
+    else if (e.keyCode === 13) playPauseMedia();
     else if(!isNaN(e.key)) year += e.key;
-    updateBC();
-    yearElement.innerHTML = year;
+
+    updateUi(year);
 }
 
 function startAnimation() {
-    runAnimation.interval = 1 / 400;
-    runAnimation.ticks = 0;
-    runAnimation.timeout = 100;
-    runAnimation.diff = 10000;
     runAnimation.startValue = 2022;
+    let endYear = runAnimation.endValue = Number(year);
+    year = "";
+    runAnimation.ticks = 0;
+    runAnimation.interval = 1 / 400;
+    runAnimation.timeout = 100;
+    runAnimation.diff = endYear - runAnimation.startValue;
+    runAnimation.future = endYear > runAnimation.startValue;
+    runAnimation.diff = Math.abs(runAnimation.diff);
     runAnimation();
 }
 
 function runAnimation() {
     if (!run) return;
-    if (runAnimation.ticks >= 1) return;
-    runAnimation.ticks = runAnimation.ticks + runAnimation.interval;
-    
-    let year = Math.round(runAnimation.startValue - (ParametricBlend(runAnimation.ticks) * runAnimation.diff));
-    if (year < 0) {
-        bc = true;
-        year = Math.abs(year);
-    } else {
-        bc = false;
+    if (runAnimation.ticks >= 1) {
+        stopMedia();
+        updateUi(runAnimation.endValue)
+        return;
     }
-    updateBC();
-    yearElement.innerHTML = year;
+
+    runAnimation.ticks = runAnimation.ticks + runAnimation.interval;
+    let diff = (ParametricBlend(runAnimation.ticks) * runAnimation.diff);
+    let calculated = runAnimation.future ? runAnimation.startValue + diff : runAnimation.startValue - diff;
+    let calculatedYear = Math.round(calculated);
+
+    updateUi(calculatedYear);
+
+    if (runAnimation.ticks > 0.8) media.playbackRate = 0.5;
+    if (runAnimation.ticks > 0.9) media.playbackRate = 0.2;
+
     setTimeout(runAnimation, runAnimation.timeout);
 }
 
-function updateBC() {
-    bcElement.innerHTML = bc ? 'BC' : 'AC';
+function updateUi(value) {
+    let bc = value < 0;
+    yearElement.innerHTML = Math.abs(value);
+    bcElement.innerHTML = bc ? '-' : '';
     if (bc) dateElement.classList.add('bc');
     else dateElement.classList.remove('bc');
 }
